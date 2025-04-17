d5cc5f26c8ed428eee1a4336139955e1174c561f
Bharath Kumar <kbharath840@gmail.com>
2025-04-17 21:19:39 +0530

    project

--- Project/Student_management_system/main.py
+++ Project/Student_management_system/main.py
@@ -1,9 +1,16 @@
-from student_management import add_student, load_data
+from student_management import *
 
 def display_menu():
     print("========Student Management System================")
     print("Operations we support, Just give the opertion number")
+    print('Press 6 to exit the application')
     print('1. Add Student')
+    print('2. View Students')
+    print('3. Search Student by ID')
+    print('4. Update Details')
+    print('5. Delete Student')
+    print('6. Course Statistics')
+    print('7. Exit')
 
 def main():
     database = load_data()
@@ -12,9 +19,20 @@ def main():
         choice = input('Enter the choice : ')
         if choice == '1':
             add_student(database)
-        if choice != '1':
+        elif choice == '2':
+            view_students(database)
+        elif choice=='3':
+            search_student(database)
+        elif choice == '4':
+            update_details(database)
+        elif choice == '5':
+            delete_student(database)
+        elif choice == '6':
+            course_statistics(database)
+        elif choice == '7':
             break
 
+#CRUD operation
 
 if __name__ == "__main__":
     main()
--- Project/Student_management_system/student_management.py
+++ Project/Student_management_system/student_management.py
@@ -49,8 +49,78 @@ def add_student(student_db):
         print(str(e))
         print('Issue in adding new student')
 
+def view_students(student_db):
+    #whether there are students in database
+    #checking whether the students_db is having atleast 1 value
+    #if student_db is empty
+    if not student_db: #if not False
+        print('No students found')
+        return None
+    for key,val in student_db.items():
+        #to get the value in dictionary
+        #dictionary_variable[key]
+        # student_db[key]['Name']
+        print("Id: : {},Name: {} ,Age : {}, Course:{},Marks:{} ".format(key,val['Name'],val['Age'],val['Course'],val['Marks']))
+
+def search_student(student_db):
+    id = input('Enter Students Id : ')
+    if id in student_db:
+        print(student_db[id])
+    else:
+        print('Student Id not found')
+
+def update_details(student_db):
+    try:
+        id = input('Enter Students Id : ')
+        if id not in student_db:
+            print('Student Id is not found')
+        else:
+            name = input('Enter the student"s name : ')
+            age = int(input('Enter Student Age : '))
+            course = input('Enter the course : ')
+            marks = float(input('Enter the marks : '))
+
+            #dictionary_variable[key] = value
+            student_db[id] = {"Name":name,
+                                        "Age":age,
+                                        "Course":course,
+                                        "Marks":marks}
+        
+            status = save_data(student_db)
+            if status == 'Success':
+                print('Student details updated successfully')
+            else:
+                print('Student details throwed error while updating')
+    except Exception as e:
+        print(str(e))
+        print('Student details throwed error while updating')
+
+def delete_student(student_db):
+    id = input('Enter student Id : ')
+    if id not in student_db:
+        print('StudentId not found')
+    else:
+        del student_db[id]
+        status = save_data(student_db)
+        if status == 'Success':
+            print('Student detail deleted successfully')
+        else:
+            print('Student details throwed error while deleting')
+        
+def course_statistics(student_db):
+    if not student_db:
+        print('No details in database')
+        return None
+    stats = {}
+    for key,val in student_db.items():
+        course_name = val['Course']
+        if course_name in stats:
+            #assigning = getting the value
+            stats[course_name] = stats[course_name] + 1
+        else:
+            stats[course_name] = 1
+    print(stats)
 
 
-      
-      
+
    
